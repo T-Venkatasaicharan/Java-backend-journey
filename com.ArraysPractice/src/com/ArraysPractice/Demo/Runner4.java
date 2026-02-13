f39cc9fd8d8145f1a4c520cb3ab0dcc7fe60eebd
Saicharan <saicharan.t27@gmail.com>
2026-02-13 12:22:15 +0530

    this is ARRAYS logic code

---
+++ com.ArraysPractice/src/com/ArraysPractice/Demo/Runner4.java
@@ -0,0 +1,31 @@
+package com.ArraysPractice.Demo;
+class Sum{
+	int sum;
+	int[] arr = {10,2,30,40};
+	int large = 0;
+	Sum(){
+	int[] arr = {10,2,30,40};
+	for(int i = 0; i < arr.length;i++) {
+		sum = sum + arr[i];
+	}
+	System.out.println(sum);
+}
+	int Large(){
+		for(int i = 0;i < arr.length;i++) {
+			if(arr[i] > large) {
+				large = arr[i];
+			}
+		}
+		return large;
+		
+	}
+}
+public class Runner4 {
+	public static void main(String[] args) {
+		Sum s = new Sum();
+		//Sum ss = new Sum();
+		s.Large();
+		System.out.println(s.large);
+	}
+
+}
